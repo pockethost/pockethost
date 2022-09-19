9d69582dac7b4b3d8e9d17439353aaab0cacaf44
Ben Allfree <ben@benallfree.com>
2022-09-19 11:16:17 -0700

    import aliases

--- packages/pockethost.io/svelte.config.js
+++ packages/pockethost.io/svelte.config.js
@@ -3,12 +3,19 @@ import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 
-  kit: {
-    adapter: adapter({ out: 'dist-server' })  },
+	kit: {
+		adapter: adapter({ out: 'dist-server' }),
+		alias: {
+			$components: './src/components',
+			$util: './src/util',
+			$src: './src'
+		}
+	},
+	target: '#svelte'
 }
 
 export default config
