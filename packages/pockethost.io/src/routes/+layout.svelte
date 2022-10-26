3411ae51de99d7e389f4f481e28e36aa4e1c1ff2
Ben Allfree <ben@benallfree.com>
2022-10-26 09:03:22 -0700

    wip

--- packages/pockethost.io/src/routes/+layout.svelte
+++ packages/pockethost.io/src/routes/+layout.svelte
@@ -2,6 +2,21 @@
   import Navbar from '$components/Navbar.svelte'
 </script>
 
+<svelte:head>
+  <script>
+    {
+      const THEME_ATTRIBUTE = 'data-theme'
+      const currentTheme =
+        document.cookie
+          .split('; ')
+          .find((row) => row.startsWith('theme='))
+          ?.split('=')?.[1] || 'dark'
+      console.log(`Current theme is ${currentTheme}`)
+      document.querySelector('html')?.setAttribute(THEME_ATTRIBUTE, currentTheme)
+      console.log(document.querySelector('html'))
+    }
+  </script>
+</svelte:head>
 <Navbar />
 
 <main data-sveltekit-prefetch>
