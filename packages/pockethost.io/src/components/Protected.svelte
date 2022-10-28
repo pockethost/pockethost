ab22742f79bdaa4a44cc5a82cd5fe1e4ee714ede
Brewhouse Digital <zach@brewhouse.digital>
2022-10-27 21:28:09 -0500

    Pushed out possible solution for the <Protected> component

--- packages/pockethost.io/src/components/Protected.svelte
+++ packages/pockethost.io/src/components/Protected.svelte
@@ -1,16 +1,7 @@
 <script lang="ts">
-  import { browser } from '$app/environment'
-  import { client } from '$src/pocketbase'
-  import { goto } from '$app/navigation'
-
-  const { isLoggedIn } = client
-
-  if (browser && !isLoggedIn()) {
-    goto(`/signup`)
-  }
+	import {isUserLoggedIn} from "$util/stores";
 </script>
 
-<slot />
-
-<style lang="scss">
-</style>
+{#if $isUserLoggedIn}
+	<slot />
+{/if}
