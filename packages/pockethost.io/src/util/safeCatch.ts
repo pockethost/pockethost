8908176d17634c7d951934a5f1712088befd56c0
Ben Allfree <ben@benallfree.com>
2022-10-29 07:40:17 -0700

    only print safeCatch messages in dev mode

--- packages/pockethost.io/src/util/safeCatch.ts
+++ packages/pockethost.io/src/util/safeCatch.ts
@@ -1,9 +1,13 @@
+import { dev } from '$app/environment'
+
 export const safeCatch = <TIn extends any[], TOut>(
   name: string,
   cb: (...args: TIn) => Promise<TOut>
 ) => {
   return (...args: TIn) => {
-    console.log(`${name}`)
+    if (dev) {
+      console.log(`${name}`)
+    }
     return cb(...args).catch((e: any) => {
       console.error(`${name} failed: ${e}`)
       throw e
