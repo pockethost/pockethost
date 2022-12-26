94ca9e0dfb6203b111ce59114c491322476277b6
Ben Allfree <ben@benallfree.com>
2022-12-26 04:17:53 -0800

    fix 500 local dev error

--- packages/pockethost.io/src/env.ts
+++ packages/pockethost.io/src/env.ts
@@ -1,3 +1,4 @@
+import { browser } from '$app/environment'
 import { env as _env } from '$env/dynamic/public'
 import { boolean } from 'boolean'
 import base from '../../../package.json'
@@ -16,7 +17,10 @@ export const PUBLIC_PB_DOMAIN = env('PUBLIC_PB_DOMAIN', 'pockethost.io')
 export const PUBLIC_PB_SUBDOMAIN = env('PUBLIC_PB_SUBDOMAIN', 'pockethost-central')
 export const PUBLIC_APP_DOMAIN = env('PUBLIC_PB_SUBDOMAIN', 'localhost')
 export const PUBLIC_APP_PROTOCOL = env('PUBLIC_APP_PROTOCOL', 'https')
-export const PUBLIC_PB_PROTOCOL = env('PUBLIC_PB_PROTOCOL', 'https')
+export const PUBLIC_PB_PROTOCOL = env(
+  'PUBLIC_PB_PROTOCOL',
+  browser ? window.location.protocol : 'https'
+)
 export const PUBLIC_DEBUG = envb('PUBLIC_DEBUG', false)
 
 export const PUBLIC_POCKETHOST_VERSION = base.version
