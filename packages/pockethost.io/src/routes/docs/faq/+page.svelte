6a880e87165384a97dc5a3035ddc3e3d9c6171b7
Oscar Fally <44458369+shaparder@users.noreply.github.com>
2023-01-24 16:08:09 +0000

    remove FAQ component from the docs

--- packages/pockethost.io/src/routes/docs/faq/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import FrequentlyAskedQuestions from "$components/FrequentlyAskedQuestions.svelte";
-</script>
-
-
-<h1>Frequently Asked Questions</h1>
-
-<FrequentlyAskedQuestions/>
