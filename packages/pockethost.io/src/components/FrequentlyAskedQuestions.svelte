06ce2499b0a58c83b41acc60a004ace03d2040ac
Ben Allfree <ben@benallfree.com>
2023-01-24 16:57:44 +0000

    chore: faq compoent formatting

--- packages/pockethost.io/src/components/FrequentlyAskedQuestions.svelte
+++ packages/pockethost.io/src/components/FrequentlyAskedQuestions.svelte
@@ -2,40 +2,38 @@
   import { faq } from '$src/docs'
   // ts interface for the Question type
   interface Question {
-    title: string,
-    body: string,
+    title: string
+    body: string
     collapsed: boolean
   }
 
   interface rawQ {
-    title: string,
+    title: string
     body: string
   }
 
   // Array of Questions, if the boolean <collapsed> is set to false, the answer is displayed
-  const questions: Question[] = faq.outline.map((q: rawQ) => ({...q, collapsed: true}))
+  const questions: Question[] = faq.outline.map((q: rawQ) => ({ ...q, collapsed: true }))
 </script>
 
 <div class="accordion w-100">
   {#each questions as question}
-  <div class="accordion-item shadow">
-    <h5 class="accordion-header">
-      <button
-        class="accordion-button title {question.collapsed ? "collapsed" : ""}"
-        type="button"
-        on:click={() => question.collapsed = !question.collapsed}
-      >
-        {question.title}
-      </button>
-    </h5>
-    <div
-      class="accordion-collapse {question.collapsed ? "collapse" : ""}"
-    >
-      <div class="accordion-body">
-        {@html question.body}
+    <div class="accordion-item shadow">
+      <h5 class="accordion-header">
+        <button
+          class="accordion-button title {question.collapsed ? 'collapsed' : ''}"
+          type="button"
+          on:click={() => (question.collapsed = !question.collapsed)}
+        >
+          {question.title}
+        </button>
+      </h5>
+      <div class="accordion-collapse {question.collapsed ? 'collapse' : ''}">
+        <div class="accordion-body">
+          {@html question.body}
+        </div>
       </div>
     </div>
-  </div>
   {/each}
 </div>
 
