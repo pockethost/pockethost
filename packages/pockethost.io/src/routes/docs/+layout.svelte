dcf4dfa3f3357e5c837de1436f1eea87d5a04ced
Ammar Iqbal <ammariqbal043@gmail.com>
2023-02-04 17:32:58 +0500

    fix sidebar

--- packages/pockethost.io/src/routes/docs/+layout.svelte
+++ packages/pockethost.io/src/routes/docs/+layout.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
+  import { page } from '$app/stores'
   import { PUBLIC_DEBUG } from '$src/env'
   import { pages } from '../../docs'
+
+  const entries = Object.entries(pages).filter(([k, p]) => p.attributes.published || PUBLIC_DEBUG)
+  $: currentURL = $page.url.pathname
 </script>
 
 <div class="docs">
   <div class="sidebar">
-    {#each Object.entries(pages).filter(([k, p]) => p.attributes.published || PUBLIC_DEBUG) as [v, k], i}
-      <div>
-        <a class={`nav-link ${k.attributes.published ? '' : 'text-danger'}`} href={`/docs/${v}`}
-          >{k.attributes.title}</a
-        >
-      </div>
+    {#each entries as [v, k], i}
+      <a
+        class={`nav-link ${k.attributes.published ? '' : 'text-danger'}`}
+        href={`/docs/${v}`}
+        class:active={currentURL.includes(v)}
+      >
+        {k.attributes.title}
+      </a>
     {/each}
   </div>
   <div class="body">
@@ -20,45 +26,45 @@
 
 <style lang="scss">
   .docs {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    display: flex;
     position: relative;
 
     .sidebar {
+      min-width: 280px;
       position: sticky;
       top: 0;
-      height: max-content;
-      background: var(--color-gray-base);
-      padding: 2rem 1rem;
+      height: 100vh;
+      background: #f8f9fa;
+      padding: 2rem;
+
+      a {
+        padding: 8px 16px;
+        font-size: 15px;
+      }
+
+      .active {
+        background-color: #ebeff2;
+      }
     }
 
     .body {
       grid-column: span 3;
-      padding-top: 2rem;
+      padding: 2rem;
+    }
+
+    @media screen and (max-width: 800px) {
+      flex-direction: column;
+      padding: 1rem;
+
+      .sidebar {
+        padding: 1rem;
+        height: 30vh;
+        overflow-y: auto;
+      }
+
+      .body {
+        padding: 2rem 0;
+      }
     }
   }
-  // .title {
-  //   border-bottom: 1px solid gray;
-  // }
-  // .container {
-  //   margin-left: initial;
-  //   padding: 10px;
-  //   white-space: nowrap;
-  //   .toc {
-  //     white-space: normal;
-  //     width: 200px;
-  //     border-right: 1px solid gray;
-  //     float: left;
-  //     margin-right: 20px;
-  //     min-height: 1000px;
-  //     div {
-  //       font-weight: bold;
-  //     }
-  //   }
-  //   .body {
-  //     white-space: normal;
-  //     float: left;
-  //     width: calc(100% - 300px);
-  //   }
-  // }
 </style>
