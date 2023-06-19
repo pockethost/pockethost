1f41a9fbf2ef2e55e007dbfe2758e9cad5e20002
Ben Allfree <ben@benallfree.com>
2023-06-19 05:12:31 -0700

    fix: miniToggle call save on action

--- packages/pockethost.io/src/components/MiniToggle.svelte
+++ packages/pockethost.io/src/components/MiniToggle.svelte
@@ -7,7 +7,7 @@
   const id = uniqueId()
   let msg = ''
 
-  $: {
+  const onChange = () => {
     save(value)
       .then((res) => {
         msg = res
@@ -19,6 +19,13 @@
 </script>
 
 <div class="form-check form-switch">
-  <input class="form-check-input" type="checkbox" role="switch" {id} bind:checked={value} />
+  <input
+    class="form-check-input"
+    type="checkbox"
+    role="switch"
+    {id}
+    bind:checked={value}
+    on:change={onChange}
+  />
   <label class="form-check-label" for={id}><slot /></label>
 </div>
