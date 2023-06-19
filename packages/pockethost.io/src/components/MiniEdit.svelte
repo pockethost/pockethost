aa8d1126afd4446c150373263f3e14898621227d
Ben Allfree <ben@benallfree.com>
2023-06-19 08:35:19 -0700

    enh: MiniEdit error feedback and input usability

--- packages/pockethost.io/src/components/MiniEdit.svelte
+++ packages/pockethost.io/src/components/MiniEdit.svelte
@@ -1,18 +1,29 @@
 <script lang="ts">
   import TinyButton from '$components/helpers/TinyButton.svelte'
+  import { logger } from '@pockethost/common'
 
   export let value: string = ''
   export let disabled: boolean = false
   export let save: (newValue: string) => Promise<string> = async () => 'saved'
 
+  const { dbg, error } = logger().create('MiniEdit.svelte')
+
   let msg = ''
+  let err = ''
   let oldValue = value
   let editedValue = value
   let editMode = false
+  let inputField: HTMLInputElement
 
   const startEdit = () => {
+    msg = ''
+    err = ''
     oldValue = editedValue
     editMode = true
+    setTimeout(() => {
+      inputField.focus()
+      inputField.select()
+    }, 0)
   }
   const cancelEdit = () => {
     editedValue = oldValue
@@ -20,13 +31,16 @@
   }
 
   const saveEdit = () => {
+    msg = ''
+    err = ''
     save(editedValue)
       .then((res) => {
         editMode = false
         msg = res
       })
       .catch((e) => {
-        msg = e.message
+        error(`Got an error on save`, e)
+        err = e.data?.data?.subdomain?.message || e.message
       })
   }
 </script>
@@ -34,10 +48,20 @@
 {#if !editMode || disabled}
   {editedValue}
   <TinyButton click={startEdit} {disabled}>edit</TinyButton>
-  {msg}
 {/if}
 {#if editMode && !disabled}
-  <input type="text" bind:value={editedValue} />
+  <input
+    bind:this={inputField}
+    type="text"
+    bind:value={editedValue}
+    on:focus={(event) => inputField.select()}
+  />
   <TinyButton style="success" {disabled} click={saveEdit}>save</TinyButton>
   <TinyButton style="danger" {disabled} click={cancelEdit}>cancel</TinyButton>
+{/if}
+{#if msg}
+  <span class="text-success">{msg}</span>
+{/if}
+{#if err}
+  <span class="text-danger">{err}</span>
 {/if}
