af6630fba444eecc43629e3ca2cf1fb44e7c74eb
Ben Allfree <ben@benallfree.com>
2023-06-21 14:47:18 +0000

    enh: certbot auto-renew

--- scripts/certbot-refresh.sh
+++ scripts/certbot-refresh.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sudo certbot --server https://acme-v02.api.letsencrypt.org/directory -d *.pockethost.io -d pockethost.io --manual --preferred-challenges dns-01 certonly
+
+# https://www.digitalocean.com/community/tutorials/how-to-create-let-s-encrypt-wildcard-certificates-with-certbot
+certbot renew --dns-digitalocean --dns-digitalocean-credentials ~/certbot-creds.ini  
+cp /etc/letsencrypt/live/pockethost.io/fullchain.pem ./ssl
+cp /etc/letsencrypt/live/pockethost.io/privkey.pem ./ssl
+chown pockethost:pockethost -R ./ssl
