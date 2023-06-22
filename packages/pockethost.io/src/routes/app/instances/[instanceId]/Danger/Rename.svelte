f46c42372ee0e1bb2588e3e1d3187101251ac4de
Isaiah Banks <isaiah.banks42@gmail.com>
2023-06-22 12:01:58 -0400

    Fix help link in rename instance section (#240)

--- packages/pockethost.io/src/routes/app/instances/[instanceId]/Danger/Rename.svelte
+++ packages/pockethost.io/src/routes/app/instances/[instanceId]/Danger/Rename.svelte
@@ -16,7 +16,7 @@
   <p class="text-danger">
     Warning - renaming your instance will cause it to become inaccessible by the old instance name.
     You also may not be able to change it back if someone else choose it. See <a
-      href="https://pockethost.gitbook.com/manual/usage/rename-instnace.md">renaming</a
+      href="https://pockethost.gitbook.io/manual/daily-usage/rename-instance">renaming</a
     > for more information.
   </p>
   <MiniEdit value={subdomain} save={onRename} />
