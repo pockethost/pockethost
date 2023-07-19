0f9150894340df76b27573651505adabcaba72ae
Ben Allfree <ben@benallfree.com>
2023-07-19 08:17:30 -0700

    fix: reactive instance subdomain in code sample section

--- packages/pockethost.io/src/routes/app/instances/[instanceId]/Code.svelte
+++ packages/pockethost.io/src/routes/app/instances/[instanceId]/Code.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
+  import AccordionItem from '$components/AccordionItem.svelte'
   import CodeSample from '$components/CodeSample.svelte'
   import { PUBLIC_APP_DOMAIN, PUBLIC_APP_PROTOCOL } from '$src/env'
-  import AccordionItem from '../../../../components/AccordionItem.svelte'
   import { instance } from './store'
 
-  $: ({ subdomain } = $instance)
-
-  const url = `${PUBLIC_APP_PROTOCOL}://${subdomain}.${PUBLIC_APP_DOMAIN}`
-  const code = `const url = '${url}'\nconst client = new PocketBase(url)`
+  let code = ''
+  $: {
+    const url = `${PUBLIC_APP_PROTOCOL}://${$instance.subdomain}.${PUBLIC_APP_DOMAIN}`
+    code = `const url = '${url}'\nconst client = new PocketBase(url)`
+  }
 </script>
 
 <AccordionItem title="Code Samples">
