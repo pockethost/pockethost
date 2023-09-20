4971ad3b4f546585d8be43269cdef4da8f34554b
Ben Allfree <ben@benallfree.com>
2023-09-20 16:42:51 +0000

    enh: launch scripts

--- scripts/pm2.sh
+++ scripts/pm2.sh
@@ -12,5 +12,7 @@ LOG_ROOT=/home/pockethost/logs
 # mv $DAEMON_LOG $LOG_ROOT/$DAEMON_PREFIX-$RESTART_DATE.log
 # echo "Server started at $RESTART_DATE" >> $DAEMON_LOG
 chown pockethost:pockethost -R $LOG_ROOT 
-pkill -f 'pocketbase serve'
+pm2 stop all
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
 yarn pm2
--- scripts/prod.sh
+++ scripts/prod.sh
@@ -4,4 +4,7 @@ set -a
 source .env
 set +a
 
+pm2 stop all
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
 yarn start
