d20b41bad42248ca4ee9ca285456a593e505866e
Ben Allfree <ben@benallfree.com>
2023-09-28 16:12:22 -0700

    singup/login fix

--- packages/dashboard/src/components/InstanceGeneratorWidget.svelte
+++ packages/dashboard/src/components/InstanceGeneratorWidget.svelte
@@ -13,13 +13,14 @@
   let formError: string = ''
 
   let isFormButtonDisabled: boolean = true
-  $: isFormButtonDisabled = email.length === 0 || password.length === 0 || instanceName.length === 0
+  $: isFormButtonDisabled =
+    email.length === 0 || password.length === 0 || instanceName.length === 0
 
   let isProcessing: boolean = false
 
   // Fun quotes when waiting for the instance to load. This could take up to 10 seconds
   let processingQuotesArray = [
-    'Did you know it takes fourteen sentient robots to create each instance on PocketHost?'
+    'Did you know it takes fourteen sentient robots to create each instance on PocketHost?',
   ]
 
   let processingQuote = getRandomElementFromArray(processingQuotesArray)
@@ -34,9 +35,14 @@
     isFormButtonDisabled = true
     isProcessing = true
 
-    await handleInstanceGeneratorWidget(email, password, instanceName, (error) => {
-      formError = error
-    })
+    await handleInstanceGeneratorWidget(
+      email,
+      password,
+      instanceName,
+      (error) => {
+        formError = error
+      },
+    )
 
     isFormButtonDisabled = false
 
@@ -44,111 +50,98 @@
   }
 </script>
 
-{#if isProcessing}
-  <div class="d-flex align-items-center gap-3">
-    <div class="spinner-border text-primary" role="status">
-      <span class="visually-hidden">Loading...</span>
-    </div>
+<div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card-body">
+    {#if isProcessing}
+      <div class="d-flex align-items-center gap-3">
+        <div class="spinner-border text-primary" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
 
-    <div>
-      <h3 class="mb-1">Creating Your New Instance...</h3>
+        <div>
+          <h2>Creating Your New Instance...</h2>
 
-      <p class="small text-muted mb-0">{processingQuote}</p>
-    </div>
-  </div>
-{:else}
-  <h3 class="mb-3">Create Your Instance Now</h3>
-
-  <form class="row align-items-center" on:submit={handleSubmit}>
-    <div class="col-lg-6 col-12">
-      <div class="form-floating mb-3 mb-lg-3">
-        <input
-          type="email"
-          class="form-control"
-          id="email"
-          placeholder="name@example.com"
-          autocomplete="email"
-          bind:value={email}
-          required
-        />
-        <label for="email">Email</label>
+          <p class="small text-muted mb-0">{processingQuote}</p>
+        </div>
       </div>
-    </div>
+    {:else}
+      <h2 class="card-title m-3">Create Your First Instance Now</h2>
 
-    <div class="col-lg-6 col-12">
-      <div class="form-floating mb-3 mb-lg-3">
-        <input
-          type="password"
-          class="form-control"
-          id="password"
-          placeholder="Password"
-          autocomplete="new-password"
-          bind:value={password}
-          required
-        />
-        <label for="password">Password</label>
-      </div>
-    </div>
+      <form class="row align-items-center" on:submit={handleSubmit}>
+        <div class="col-lg-6 col-12">
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              class="input input-bordered w-full max-w-xs"
+              id="email"
+              placeholder="name@example.com"
+              autocomplete="email"
+              bind:value={email}
+              required
+            />
+          </div>
+        </div>
 
-    <div class="col-lg-6 col-12">
-      <div class="form-floating mb-3 mb-lg-3">
-        <input
-          type="text"
-          class="form-control"
-          id="instance"
-          placeholder="Instance"
-          bind:value={instanceName}
-          required
-        />
-        <label for="instance">Instance Name</label>
+        <div class="col-lg-6 col-12">
+          <div class="form-floating mb-3 mb-lg-3">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              class="input input-bordered w-full max-w-xs"
+              id="password"
+              placeholder="Password"
+              autocomplete="new-password"
+              bind:value={password}
+              required
+            />
+          </div>
+        </div>
 
-        <button
-          aria-label="Regenerate Instance Name"
-          type="button"
-          style="transform: rotate({rotationCounter}deg);"
-          class="btn btn-light rounded-circle regenerate-instance-name-btn"
-          on:click={handleInstanceNameRegeneration}
-        >
-          <i class="bi bi-arrow-repeat" />
-        </button>
-      </div>
-    </div>
+        <div class="col-lg-6 col-12">
+          <div class="form-floating mb-3 mb-lg-3">
+            <label class="label">
+              <span class="label-text">Instance Name</span>
+            </label>
+            <input
+              type="text"
+              class="input input-bordered w-full max-w-xs"
+              id="instance"
+              placeholder="Instance"
+              bind:value={instanceName}
+              required
+            />
 
-    <div class="col-lg-6 col-12">
-      <div class="mb-3 mb-lg-3 text-lg-start text-center">
-        <button type="submit" class="btn btn-primary" disabled={isFormButtonDisabled}>
-          Create <i class="bi bi-arrow-right-short" />
-        </button>
-      </div>
-    </div>
+            <button
+              aria-label="Regenerate Instance Name"
+              type="button"
+              style="transform: rotate({rotationCounter}deg);"
+              class="m-3 btn btn-light rounded-circle regenerate-instance-name-btn"
+              on:click={handleInstanceNameRegeneration}
+            >
+              regenerate
+            </button>
+          </div>
+        </div>
 
-    {#if formError}
-      <AlertBar icon="bi bi-exclamation-triangle-fill" text={formError} />
+        <div class="card-actions justify-end">
+          <button
+            type="submit"
+            class="btn btn-primary"
+            disabled={isFormButtonDisabled}
+          >
+            Create <i class="bi bi-arrow-right-short" />
+          </button>
+        </div>
+
+        {#if formError}
+          <AlertBar icon="bi bi-exclamation-triangle-fill" text={formError} />
+        {/if}
+      </form>
     {/if}
-  </form>
-{/if}
-
-<style>
-  form {
-    max-width: 600px;
-  }
-
-  .row {
-    --bs-gutter-x: 0.5rem;
-  }
-
-  .btn.btn-primary {
-    --bs-btn-padding-y: 12px;
-  }
-
-  .regenerate-instance-name-btn {
-    padding: 0;
-    width: 40px;
-    height: 40px;
-    position: absolute;
-    z-index: 500;
-    top: 10px;
-    right: 7px;
-    transition: all 200ms;
-  }
-</style>
+  </div>
+</div>
--- packages/dashboard/src/routes/login/+page.svelte
+++ packages/dashboard/src/routes/login/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { handleLogin } from '$util/database'
   import AlertBar from '$components/AlertBar.svelte'
+  import { handleLogin } from '$util/database'
 
   let email: string = ''
   let password: string = ''
@@ -26,86 +26,59 @@
   <title>Sign In - PocketHost</title>
 </svelte:head>
 
-<div class="page-bg">
-  <div class="card">
-    <h2 class="mb-4">Login</h2>
+<div class="flex justify-center">
+  <div class="card w-96 bg-base-100 shadow-xl">
+    <div class="card-body">
+      <h2 class="card-title mb-4">Login</h2>
 
-    <form on:submit={handleSubmit}>
-      <div class="form-floating mb-3">
-        <input
-          type="email"
-          class="form-control"
-          id="email"
-          placeholder="name@example.com"
-          bind:value={email}
-          required
-          autocomplete="email"
-        />
-        <label for="email">Email address</label>
+      <form on:submit={handleSubmit}>
+        <div class="form-control w-full max-w-xs">
+          <label class="label" for="email">Email address</label>
+          <input
+            type="email"
+            class="input input-bordered w-full max-w-xs"
+            id="email"
+            placeholder="name@example.com"
+            bind:value={email}
+            required
+            autocomplete="email"
+          />
+        </div>
+
+        <div class="form-control w-full max-w-xs">
+          <label class="label" for="password">Password</label>
+          <input
+            type="password"
+            class="input input-bordered w-full max-w-xs"
+            id="password"
+            placeholder="Password"
+            bind:value={password}
+            required
+            autocomplete="current-password"
+          />
+        </div>
+
+        <p class="m-3"></p>
+
+        {#if formError}
+          <AlertBar icon="bi bi-exclamation-triangle-fill" text={formError} />
+        {/if}
+
+        <button
+          type="submit"
+          class="btn btn-primary w-100"
+          disabled={isFormButtonDisabled}
+        >
+          Log In <i class="bi bi-arrow-right-short" />
+        </button>
+      </form>
+
+      <div class="py-4"><hr /></div>
+
+      <div class="text-center">
+        <a class="link" href="/signup">sign up</a> |
+        <a class="link" href="/login/password-reset">forgot password</a>
       </div>
-
-      <div class="form-floating mb-3">
-        <input
-          type="password"
-          class="form-control"
-          id="password"
-          placeholder="Password"
-          bind:value={password}
-          required
-          autocomplete="current-password"
-        />
-        <label for="password">Password</label>
-      </div>
-
-      <p class="password-reset-container">
-        <a href="/login/password-reset">Forgot Your Password?</a>
-      </p>
-
-      {#if formError}
-        <AlertBar icon="bi bi-exclamation-triangle-fill" text={formError} />
-      {/if}
-
-      <button type="submit" class="btn btn-primary w-100" disabled={isFormButtonDisabled}>
-        Log In <i class="bi bi-arrow-right-short" />
-      </button>
-    </form>
-
-    <div class="py-4"><hr /></div>
-
-    <div class="text-center">
-      Need to <a href="/signup">create an account</a>?
     </div>
   </div>
 </div>
-
-<style lang="scss">
-  .page-bg {
-    background-color: #222;
-    background-image: var(--gradient-light-soft-blue);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: calc(100vh - 91px);
-    padding: 0 18px;
-  }
-
-  .card {
-    border: 0;
-    box-shadow: var(--soft-box-shadow);
-    padding: 24px;
-    max-width: 425px;
-    width: 100%;
-    border-radius: 24px;
-  }
-
-  .password-reset-container {
-    font-size: 13px;
-    text-align: right;
-  }
-
-  @media screen and (min-width: 768px) {
-    .card {
-      padding: 48px;
-    }
-  }
-</style>
