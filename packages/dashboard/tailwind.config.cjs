982e12bf493d8a78da3762a91da1f57902b34c8f
Ben Allfree <ben@benallfree.com>
2023-09-28 15:47:39 -0700

    wip

--- packages/dashboard/tailwind.config.cjs
+++ packages/dashboard/tailwind.config.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-  content: ['./src/routes/**/*.{svelte,js,ts}'],
+  content: ['./src/**/*.{svelte,js,ts}'],
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
 }
