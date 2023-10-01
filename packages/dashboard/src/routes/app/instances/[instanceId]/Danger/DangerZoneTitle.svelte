9d194182d7c7164ea10921dcd697a891c57c9783
Zach <zachary.voss@gmail.com>
2023-10-01 00:18:37 -0500

    Set the Danger Zone text to red

--- packages/dashboard/src/routes/app/instances/[instanceId]/Danger/DangerZoneTitle.svelte
+++ packages/dashboard/src/routes/app/instances/[instanceId]/Danger/DangerZoneTitle.svelte
@@ -1,9 +1,9 @@
 <div class="block py-8 mb-4">
   <div class='flex items-center justify-center gap-4 w-full'>
-    <i class="fa-solid fa-siren-on"></i>
+    <i class="fa-solid fa-siren-on text-red-600"></i>
 
-    <h2 class='text-4xl font-bold'>Danger Zone</h2>
+    <h2 class='text-4xl font-bold text-red-600'>Danger Zone</h2>
 
-    <i class="fa-solid fa-siren-on"></i>
+    <i class="fa-solid fa-siren-on text-red-600"></i>
   </div>
 </div>
