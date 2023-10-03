c12a252890fce99d945b5b8abc08dfac9c2c8809
Zach <zachary.voss@gmail.com>
2023-10-03 12:16:22 -0500

    Fixed issue with double title, and updated the description tags

--- packages/www/_data/metadata.js
+++ packages/www/_data/metadata.js
@@ -3,5 +3,5 @@ module.exports = {
   url: 'https://pockethost.io',
   language: 'en',
   description:
-    'Get a PocketBase backend for your next app in under 10 seconds.',
+    'Get a PocketBase backend for your next app in under 30 seconds.',
 }
