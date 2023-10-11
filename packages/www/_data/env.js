1efd71de007be85a986803665018e060c04fb7b3
Ben Allfree <ben@benallfree.com>
2023-10-11 05:50:49 -0700

    fix: revert env vars to default to prod settings

--- packages/www/_data/env.js
+++ packages/www/_data/env.js
@@ -1,7 +1,7 @@
 const env = {
   HTTP_PROTOCOL: process.env.PUBLIC_HTTP_PROTOCOL || 'https',
-  BLOG_DOMAIN: process.env.PUBLIC_BLOG_DOMAIN || 'pockethost.lvh.me',
-  APP_DOMAIN: process.env.PUBLIC_APP_DOMAIN || 'app.pockethost.lvh.me',
+  BLOG_DOMAIN: process.env.PUBLIC_BLOG_DOMAIN || 'pockethost.io',
+  APP_DOMAIN: process.env.PUBLIC_APP_DOMAIN || 'app.pockethost.io',
 }
 env.APP_URL = `${env.HTTP_PROTOCOL}://${env.APP_DOMAIN}`
 env.BLOG_URL = `${env.HTTP_PROTOCOL}://${env.BLOG_DOMAIN}`
