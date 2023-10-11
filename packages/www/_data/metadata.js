ff583270dbaca4fdfd97c4af5fda4e233d92bd49
Ben Allfree <ben@benallfree.com>
2023-10-11 05:25:47 -0700

    enh: add BLOG_DOMAIN support to www project and github actions

--- packages/www/_data/metadata.js
+++ packages/www/_data/metadata.js
@@ -1,6 +1,8 @@
+const { BLOG_URL } = require('./env')
+
 module.exports = {
   title: 'PocketHost',
-  url: 'https://pockethost.io/',
+  url: BLOG_URL,
   language: 'en',
   description:
     'Get a PocketBase backend for your next app in under 10 seconds.',
