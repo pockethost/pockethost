fe04fafcaf0ecc60f60b42a4f7b9af72e913c661
Ben Allfree <ben@benallfree.com>
2023-10-11 20:28:52 -0700

    enh: www updates for improved environment variable support

--- packages/www/_data/env.js
+++ packages/www/_data/env.js
@@ -1,9 +1,35 @@
+const { boolean } = require('boolean')
+
+/**
+ * These environment variables default to pointing to the production build so frontend development is easy.
+ * If they are specified in .env, those values will prevail.
+ */
+
 const env = {
-  HTTP_PROTOCOL: process.env.PUBLIC_HTTP_PROTOCOL || 'https',
-  BLOG_DOMAIN: process.env.PUBLIC_BLOG_DOMAIN || 'pockethost.io',
-  APP_DOMAIN: process.env.PUBLIC_APP_DOMAIN || 'app.pockethost.io',
+  // The domain name of the lander/marketing site
+  PUBLIC_BLOG_DOMAIN: process.env.PUBLIC_BLOG_DOMAIN || 'pockethost.io',
+
+  // The domain name where this dashboard lives
+  PUBLIC_APP_DOMAIN: process.env.PUBLIC_APP_DOMAIN || 'app.pockethost.io',
+
+  // The domain name apex where all instances live (eg, <subdomain>.pockethost.io)
+  PUBLIC_EDGE_APEX_DOMAIN:
+    process.env.PUBLIC_EDGE_APEX_DOMAIN || 'pockethost.io',
+
+  // The protocol to use, almost always will be https
+  PUBLIC_HTTP_PROTOCOL: process.env.PUBLIC_HTTP_PROTOCOL || 'https',
+
+  // The complete URL to the mothership
+  PUBLIC_MOTHERSHIP_NAME:
+    process.env.PUBLIC_MOTHERSHIP_NAME || `pockethost-central`,
+
+  // Whether or not we are in debugging mode - default TRUE
+  PUBLIC_DEBUG: boolean(process.env.PUBLIC_DEBUG || 'true'),
 }
-env.APP_URL = `${env.HTTP_PROTOCOL}://${env.APP_DOMAIN}`
-env.BLOG_URL = `${env.HTTP_PROTOCOL}://${env.BLOG_DOMAIN}`
+
+// Derived values
+env.APP_URL = `${env.PUBLIC_HTTP_PROTOCOL}://${env.PUBLIC_APP_DOMAIN}`
+env.BLOG_URL = `${env.PUBLIC_HTTP_PROTOCOL}://${env.PUBLIC_BLOG_DOMAIN}`
+env.MOTHERSHIP_URL = `${env.PUBLIC_HTTP_PROTOCOL}://${env.PUBLIC_MOTHERSHIP_NAME}.${this.PUBLIC_EDGE_APEX_DOMAIN}`
 
 module.exports = env
