cd52f924ac2db8ce5b71322f7911a4191a09df0b
Ben Allfree <ben@benallfree.com>
2023-10-18 10:41:09 -0700

    enh: added common assert util

--- packages/common/src/assert.ts
+++ packages/common/src/assert.ts
@@ -15,3 +15,12 @@ export function assertTruthy<TType>(
     throw new Error(message)
   }
 }
+
+export function assert<T>(
+  v: T | undefined | void | null,
+  msg?: string,
+): asserts v is T {
+  if (!v) {
+    throw new Error(msg || `Assertion failure`)
+  }
+}
