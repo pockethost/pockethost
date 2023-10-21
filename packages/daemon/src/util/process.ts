3bb058762abe014d6672d8279aaa115d631e30b7
Ben Allfree <ben@benallfree.com>
2023-10-20 17:45:08 -0700

    fix: throw error on debug for unhandled rejections and exceptions

--- packages/daemon/src/util/process.ts
+++ packages/daemon/src/util/process.ts
@@ -1,6 +1,5 @@
 import { PUBLIC_DEBUG } from '$constants'
 import { LoggerService } from '@pockethost/common'
-import { gracefulExit } from './exit'
 ;['unhandledRejection', 'uncaughtException'].forEach((type) => {
   process.on(type, (e) => {
     const { error } = LoggerService().create(type)
@@ -8,7 +7,7 @@ import { gracefulExit } from './exit'
     error(`${e}`)
 
     if (PUBLIC_DEBUG) {
-      gracefulExit(42)
+      throw e
     }
   })
 })
