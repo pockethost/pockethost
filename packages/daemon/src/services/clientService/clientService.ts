f5cf75322c2cc5f3705fafa219378f59dee8a430
Ben Allfree <ben@benallfree.com>
2023-10-21 06:16:28 -0700

    fix: continuously try to log in as admin

--- packages/daemon/src/services/clientService/clientService.ts
+++ packages/daemon/src/services/clientService/clientService.ts
@@ -13,19 +13,21 @@ export const clientService = mkSingleton(async (cfg: ClientServiceConfig) => {
   const { dbg, error } = _clientLogger
   const client = createPbClient(url)
 
-  try {
-    await client.adminAuthViaEmail(username, password)
-    dbg(`Logged in as admin`)
-  } catch (e) {
-    dbg(`Creating first admin account`)
-
+  while (true) {
     try {
-      await client.createFirstAdmin(username, password)
       await client.adminAuthViaEmail(username, password)
-      dbg(`Logged in`)
+      dbg(`Logged in as admin`)
+      break
     } catch (e) {
-      error(`CANNOT AUTHENTICATE TO ${url}`)
-      throw e
+      dbg(`Creating first admin account`)
+
+      try {
+        await client.createFirstAdmin(username, password)
+        await client.adminAuthViaEmail(username, password)
+        dbg(`Logged in`)
+      } catch (e) {
+        error(`CANNOT AUTHENTICATE TO ${url}`)
+      }
     }
   }
 
