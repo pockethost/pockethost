8c98dec2a1dc2aca7b3bbc373c8e28a8ecbe29ce
Ben Allfree <ben@benallfree.com>
2023-12-11 06:19:54 -0800

    fix: live sync script

--- scripts/live-sync.sh
+++ scripts/live-sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Used to back up to an encrypted drive
-rsync -avz --progress --exclude 'logs.db' --exclude 'instance_logs.db' --exclude '*.db-shm' --exclude '*.db-wal' --exclude 'pb_data/types.d.ts'  pockethost.io:/home/pockethost/data/ live-data
+rsync -avz --progress --exclude 'logs.db' --exclude 'instance_logs.db' --exclude '*.db-shm' --exclude '*.db-wal' --exclude 'pb_data/types.d.ts'  pockethost.io:/home/pockethost/data/ .pockethost/live-data
