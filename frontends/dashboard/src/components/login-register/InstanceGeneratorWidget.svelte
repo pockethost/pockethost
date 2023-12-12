48a2bdae30f71a136deb383ecb21a6a3bac866b8
Ben Allfree <ben@benallfree.com>
2023-12-12 05:51:25 -0800

    fix registration error

--- frontends/dashboard/src/components/login-register/InstanceGeneratorWidget.svelte
+++ frontends/dashboard/src/components/login-register/InstanceGeneratorWidget.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
   import LoginForm from '$components/login-register/LoginForm.svelte'
-  import NewInstanceProcessingBlock from '$components/login-register/NewInstanceProcessingBlock.svelte'
   import RegisterForm from '$components/login-register/RegisterForm.svelte'
   import { onMount } from 'svelte'
   import { slide } from 'svelte/transition'
 
   // Create a toggle to hold the Sign-Up view or the Register view
   let isSignUpView: boolean = true
-
-  // Disable the form button while the instance is being created
-  let isProcessing: boolean = false
 
   onMount(() => {
     // Check the url if the parameter "view" is set and change the form view
@@ -26,15 +22,7 @@
 <div class="card w-96 bg-zinc-900 mx-auto shadow-xl overflow-hidden">
   {#if isSignUpView}
     <div in:slide={{ delay: 400 }} out:slide>
-      {#if isProcessing}
-        <div in:slide={{ delay: 400 }} out:slide>
-          <NewInstanceProcessingBlock />
-        </div>
-      {:else}
-        <div in:slide={{ delay: 400 }} out:slide>
-          <RegisterForm bind:isProcessing bind:isSignUpView />
-        </div>
-      {/if}
+      <RegisterForm bind:isSignUpView />
     </div>
   {/if}
 
