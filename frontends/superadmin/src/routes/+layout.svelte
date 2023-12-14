460a1aaa18e84bb57d6c74d7f15bb23fe8f0327a
Ben Allfree <ben@benallfree.com>
2023-12-13 23:46:37 -0800

    More superadmin nav fixes

--- frontends/superadmin/src/routes/+layout.svelte
+++ frontends/superadmin/src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script>
   import { page } from '$app/stores'
   import Logo from '$components/Logo.svelte'
-  import MediaQuery from '$components/MediaQuery.svelte'
-  import MobileNavDrawer from '$components/MobileNavDrawer.svelte'
-  import Navbar from '$components/Navbar.svelte'
   import AuthStateGuard from '$components/helpers/AuthStateGuard.svelte'
   import Meta from '$components/helpers/Meta.svelte'
   import UserLoggedIn from '$components/helpers/UserLoggedIn.svelte'
@@ -25,62 +22,34 @@
 
 <AuthStateGuard>
   <div class="layout xl:flex">
-    <UserLoggedIn>
-      <MediaQuery query="(min-width: 1280px)" let:matches>
-        {#if matches}
-          <Navbar />
-        {/if}
-      </MediaQuery>
-
-      <MediaQuery query="(max-width: 700px)" let:matches>
-        {#if matches}
-          <MobileNavDrawer>
-            <Navbar />
-          </MobileNavDrawer>
-        {/if}
-      </MediaQuery>
-    </UserLoggedIn>
-
     <div class="p-0 lg:p-4 min-h-screen grow">
       <div
         class="bg-base-300 border-base-300 border-[16px] m-0 md:p-4 rounded-2xl"
       >
-        <MediaQuery
-          query="(min-width: 701px) and (max-width: 1279px)"
-          let:matches
-        >
-          {#if matches}
-            <UserLoggedIn>
-              <div role="tablist" class="tabs tabs-boxed">
-                <a href="/" role="tab" class="tab">
-                  <Logo hideLogoText={true} logoWidth="h-8" />
-                </a>
+        <UserLoggedIn>
+          <div role="tablist" class="tabs tabs-boxed">
+            <a href="/" role="tab" class="tab">
+              <Logo hideLogoText={true} logoWidth="h-8" />
+            </a>
 
-                <a
-                  role="tab"
-                  class="tab {$page.url.pathname === '/stats'
-                    ? `tab-active`
-                    : ``}"
-                  href="/stats">Stats</a
-                >
-                <a
-                  role="tab"
-                  class="tab {$page.url.pathname === '/plugins'
-                    ? `tab-active`
-                    : ``}"
-                  href="/plugins">Plugins</a
-                >
+            <a
+              role="tab"
+              class="tab {$page.url.pathname === '/stats' ? `tab-active` : ``}"
+              href="/stats">Stats</a
+            >
+            <a
+              role="tab"
+              class="tab {$page.url.pathname === '/plugins'
+                ? `tab-active`
+                : ``}"
+              href="/plugins">Plugins</a
+            >
 
-                <button
-                  type="button"
-                  class="tab"
-                  on:click={handleLogoutAndRedirect}
-                  ><i class="fa-regular fa-arrow-up-left-from-circle mr-2"></i> Logout</button
-                >
-              </div>
-            </UserLoggedIn>
-          {/if}
-        </MediaQuery>
+            <button type="button" class="tab" on:click={handleLogoutAndRedirect}
+              ><i class="fa-regular fa-arrow-up-left-from-circle mr-2"></i> Logout</button
+            >
+          </div>
+        </UserLoggedIn>
 
         <slot />
       </div>
