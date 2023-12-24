bec9a3d247317abb48a2522452fee298307e7849
Brewhouse Digital <66521220+brewhousedigital@users.noreply.github.com>
2023-12-23 22:50:01 -0600

    Dashboard - Added first round Pricing Page

--- frontends/dashboard/src/routes/account/+page.svelte
+++ frontends/dashboard/src/routes/account/+page.svelte
@@ -1,18 +1,19 @@
-<script>
+<script lang="ts">
   import AuthStateGuard from '$components/helpers/AuthStateGuard.svelte'
+  import FAQItem from '$src/routes/account/FAQItem.svelte'
+  import PricingCard from '$src/routes/account/PricingCard.svelte'
   import { PLAN_NAMES, SubscriptionType } from '$shared'
   import { DISCORD_URL } from '$src/env'
   import { client } from '$src/pocketbase-client'
-  import {
-    isUserFounder,
-    isUserLegacy,
-    userSubscriptionType,
-  } from '$util/stores'
+  import { isUserLegacy, userSubscriptionType } from '$util/stores'
   import { onMount } from 'svelte'
   import { writable } from 'svelte/store'
   import Card from './Card.svelte'
 
   const founderMembershipsRemaining = writable(0)
+
+  /** @type {"monthly"|"annually"} */
+  let planSchedule = 'month'
 
   onMount(async () => {
     try {
@@ -27,106 +28,150 @@
 </script>
 
 <AuthStateGuard>
-  <div class="m-10">
-    <h2 class="text-2xl">My Account</h2>
-    <h2>Current Plan: {PLAN_NAMES[$userSubscriptionType]}</h2>
-    <h2>Email: {client().user()?.email}</h2>
-  </div>
+  <div class="">
+    <main>
+      <div class="mx-auto mt-16 max-w-7xl px-6 sm:mt-32 lg:px-8">
+        <div class="mx-auto max-w-4xl text-center">
+          <h1 class="text-base font-semibold leading-7 text-indigo-400">
+            Pricing
+          </h1>
+          <p
+            class="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl"
+          >
+            Pricing plans for teams of&nbsp;all&nbsp;sizes
+          </p>
+        </div>
 
-  {#if $isUserLegacy}
-    <Card
-      name={PLAN_NAMES[SubscriptionType.Legacy]}
-      active={$userSubscriptionType === SubscriptionType.Legacy}
-      features={[
-        `Access to existing projects and legacy features`,
-        `Unlimited (fair use) bandwith, storage, and CPU`,
-        `Community support via Discord`,
-        `FTP access to PocketBase data, hooks, migrations, and files`,
-        `Run every version of PocketBase`,
-      ]}
-    />
-  {:else}
-    <Card
-      name={PLAN_NAMES[SubscriptionType.Free]}
-      active={$userSubscriptionType === SubscriptionType.Free}
-      features={[
-        `1 project`,
-        `Unlimited (fair use) bandwith, storage, and CPU`,
-        `Community support via Discord`,
-        `FTP access to PocketBase data, hooks, migrations, and files`,
-        `Run every version of PocketBase`,
-      ]}
-    />
-  {/if}
+        <p
+          class="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-300"
+        >
+          Choose an affordable plan that’s packed with the best features for
+          engaging your audience, creating customer loyalty, and driving sales.
+        </p>
 
-  {#if [SubscriptionType.Free, SubscriptionType.Legacy].includes($userSubscriptionType)}
-    <Card
-      name={PLAN_NAMES[SubscriptionType.Premium]}
-      active={$userSubscriptionType === SubscriptionType.Premium}
-      features={[
-        `Everything in the free tier`,
-        `Unlimited projects`,
-        `Priority support channel on Discord`,
-      ]}
-      upgradable={[SubscriptionType.Free, SubscriptionType.Legacy].includes(
-        $userSubscriptionType,
-      )}
-      prices={[
-        { title: `$20/mo`, link: `https://buy.stripe.com/fZe6sd8Mkfc30Kc4gg` },
-        {
-          title: `$199/yr (save 20%)`,
-          link: `https://buy.stripe.com/bIYeYJbYwd3VdwY289`,
-        },
-      ]}
-    >
-      <p>
-        We have a lot of ideas for premium features. Please check our <a
-          class="link"
-          target="_blank"
-          href="https://discord.com/channels/1128192380500193370/1180219900107706409"
-          >Discord discussion</a
-        > to join in the conversation.
-      </p></Card
-    >
-  {/if}
+        <div class="mt-16 flex justify-center">
+          <fieldset
+            class="grid grid-cols-2 gap-x-1 rounded-full bg-white/5 p-1 text-center text-xs font-semibold leading-5 text-white"
+          >
+            <legend class="sr-only">Payment frequency</legend>
 
-  {#if [SubscriptionType.Free, SubscriptionType.Legacy, SubscriptionType.Lifetime].includes($userSubscriptionType) || $isUserFounder}
-    <Card
-      name={`🎉 🎈 Founder's Edition 🎊 🍄`}
-      active={$isUserFounder}
-      features={[
-        `Everything in the Pro tier`,
-        `Founder's badge on Discord`,
-        `Official PocketHost mug or tee`,
-      ]}
-      startLimit={100}
-      limit={$founderMembershipsRemaining}
-      upgradable
-      prices={[
-        {
-          title: `$99/yr (50% savings)`,
-          link: `https://buy.stripe.com/aEUdUF7Igfc350s28a`,
-        },
-        {
-          title: `$299 lifetime`,
-          link: `https://buy.stripe.com/7sIg2N6Ecgg70KcdQT`,
-        },
-      ]}
-    >
-      <p class="text-info text-xl">
-        Super elite! The Founder's Edition is our way of saying thanks for
-        supporting PocketHost in these early days.
-      </p>
-    </Card>
-  {/if}
+            <!-- Checked: "bg-indigo-500" -->
+            <label
+              class={`cursor-pointer rounded-full px-2.5 py-1 ${
+                planSchedule === 'month' && 'bg-indigo-500'
+              }`}
+            >
+              <input
+                type="radio"
+                bind:group={planSchedule}
+                name="frequency"
+                value="month"
+                class="sr-only"
+              />
+              <span>Monthly</span>
+            </label>
 
-  <div>
-    {#if $isUserLegacy}
-      <div class="m-10 inline-block">
-        <div class="card w-96 bg-base-100 shadow-xl">
-          <div class="card-body">
-            <h2 class="card-title">Legacy Account Policy</h2>
-            <p>
+            <!-- Checked: "bg-indigo-500" -->
+            <label
+              class={`cursor-pointer rounded-full px-2.5 py-1 ${
+                planSchedule === 'year' && 'bg-indigo-500'
+              }`}
+            >
+              <input
+                type="radio"
+                bind:group={planSchedule}
+                name="frequency"
+                value="year"
+                class="sr-only"
+              />
+              <span>Annually</span>
+            </label>
+          </fieldset>
+        </div>
+
+        <div
+          class="mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+        >
+          {#if $isUserLegacy}
+            <PricingCard
+              name={PLAN_NAMES[SubscriptionType.Legacy]}
+              description="The essentials to provide your best work."
+              priceMonthly={0}
+              priceAnnually={0}
+              {planSchedule}
+              active={$userSubscriptionType === SubscriptionType.Legacy}
+              features={[
+                `Access to existing projects and legacy features`,
+                `Unlimited (fair use) bandwith, storage, and CPU`,
+                `Community support via Discord`,
+                `FTP access to PocketBase data, hooks, migrations, and files`,
+                `Run every version of PocketBase`,
+              ]}
+            />
+          {:else}
+            <PricingCard
+              name={PLAN_NAMES[SubscriptionType.Free]}
+              description="The essentials to provide your best work."
+              priceMonthly={0}
+              priceAnnually={0}
+              {planSchedule}
+              active={$userSubscriptionType === SubscriptionType.Free}
+              features={[
+                `1 project`,
+                `Unlimited (fair use) bandwith, storage, and CPU`,
+                `Community support via Discord`,
+                `FTP access to PocketBase data, hooks, migrations, and files`,
+                `Run every version of PocketBase`,
+              ]}
+            />
+          {/if}
+
+          <!-- $199 Plan: https://buy.stripe.com/bIYeYJbYwd3VdwY289 -->
+          <PricingCard
+            name={PLAN_NAMES[SubscriptionType.Premium]}
+            description="The essentials to provide your best work."
+            priceMonthly={20}
+            priceAnnually={99}
+            {planSchedule}
+            checkoutMonthURL="https://buy.stripe.com/fZe6sd8Mkfc30Kc4gg"
+            checkoutYearURL="https://buy.stripe.com/aEUdUF7Igfc350s28a"
+            active={$userSubscriptionType === SubscriptionType.Premium}
+            features={[
+              `Everything in the Free plan`,
+              `Unlimited instances`,
+              `Unlimited (fair use) bandwidth, storage, and CPU`,
+            ]}
+          />
+
+          <PricingCard
+            name={PLAN_NAMES[SubscriptionType.Lifetime]}
+            description="The essentials to provide your best work."
+            priceMonthly={300}
+            priceAnnually={300}
+            {planSchedule}
+            active={$userSubscriptionType === SubscriptionType.Lifetime}
+            features={[
+              `Everything in the Free plan`,
+              `Unlimited instances`,
+              `Unlimited (fair use) bandwidth, storage, and CPU`,
+              `Founder's badge on Discord`,
+              `Official PocketHost mug or tee`,
+            ]}
+          />
+        </div>
+      </div>
+
+      <!-- FAQ section -->
+      <div
+        class="mx-auto max-w-7xl divide-y divide-gray-500 px-6 py-24 sm:py-32 lg:px-8 lg:py-40"
+      >
+        <h2 class="text-2xl font-bold leading-10 tracking-tight">
+          Frequently asked questions
+        </h2>
+
+        <dl class="mt-10 space-y-8 divide-y divide-gray-500">
+          <FAQItem question="What is the "Legacy Plan"?">
+            <p class="mb-4">
               Legacy accounts have access to existing projects and features, but
               cannot create new projects or use new features.
             </p>
@@ -136,80 +181,71 @@
               new projects and features created on a paid plan will no longer
               work.
             </p>
-          </div>
-        </div>
-      </div>
-    {/if}
+          </FAQItem>
 
-    <div class="m-10 inline-block">
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <h2 class="card-title">Giving Back to the Open Source Community</h2>
-          <p>
-            PocketHost is committed to giving back to the open source community
-            that helps create PocketHost and PocketBase.
-          </p>
-          <p>
-            10% of net proceeds (after expenses) are donated back to the
-            community. Specifically, PocketHost makes donations to the
-            PocketBase project and major contributors to the PocketHost project.
-          </p>
-          <p>
-            In addition, 1% of membership fees is collected by Stripe to reduce
-            carbon footprints around the world.
-          </p>
-        </div>
-      </div>
-    </div>
+          <FAQItem question="Giving Back to the Open Source Community">
+            <p class="mb-4">
+              PocketHost is committed to giving back to the open source
+              community that helps create PocketHost and PocketBase.
+            </p>
+            <p class="mb-4">
+              10% of net proceeds (after expenses) are donated back to the
+              community. Specifically, PocketHost makes donations to the
+              PocketBase project and major contributors to the PocketHost
+              project.
+            </p>
+            <p>
+              In addition, 1% of membership fees is collected by Stripe to
+              reduce carbon footprints around the world.
+            </p>
+          </FAQItem>
 
-    <div class="m-10 inline-block">
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <h2 class="card-title">Fair Use Policy</h2>
-          <p>
-            When we say 'unlimited', we mean it in the Fair Use sense of the
-            word. Obviously, everything has limits. In our study of PocketHost
-            usage patterns, we found that even the busiest and most successful
-            PocketHost instances rarely stress our system.
-          </p>
-          <p>
-            PocketHost is a haven for developers who want to launch and iterate
-            quickly on ideas without worrying about metering and infrastructure.
-          </p>
-          <p>
-            If your app gets big and it starts affecting the system, we'll talk
-            about an enterprise plan or a dedicated setup.
-          </p>
-          <p>
-            Please enjoy PocketHost knowing that you can use as much storage,
-            bandwidth, and CPU as your application requires under normal
-            operating conditions. Let us handle the hosting so you can get back
-            to work.
-          </p>
-        </div>
-      </div>
-    </div>
+          <FAQItem question="Fair Use Policy">
+            <p class="mb-4">
+              When we say 'unlimited', we mean it in the Fair Use sense of the
+              word. Obviously, everything has limits. In our study of PocketHost
+              usage patterns, we found that even the busiest and most successful
+              PocketHost instances rarely stress our system.
+            </p>
+            <p class="mb-4">
+              PocketHost is a haven for developers who want to launch and
+              iterate quickly on ideas without worrying about metering and
+              infrastructure.
+            </p>
+            <p class="mb-4">
+              If your app gets big and it starts affecting the system, we'll
+              talk about an enterprise plan or a dedicated setup.
+            </p>
+            <p>
+              Please enjoy PocketHost knowing that you can use as much storage,
+              bandwidth, and CPU as your application requires under normal
+              operating conditions. Let us handle the hosting so you can get
+              back to work.
+            </p>
+          </FAQItem>
 
-    <div class="m-10 inline-block">
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <h2 class="card-title">Cancellation and Refunds</h2>
-          <p>Short version: We only want your money if you are happy.</p>
-          <p>
-            Long version: If you need to cancel your membership for any reason,
-            please <a class="link" href={DISCORD_URL}
-              >contact @noaxis on Discord</a
-            >. If you cancel within the first 5 days of a signup or renewal,
-            we'll refund the full amount. Otherwise, we'll pro-rate it. Sound
-            good?
-          </p>
-          <p>
-            If you create additional instances and then downgrade to the free
-            plan, the extra instances will remain accessible in your dashboard,
-            but they will not run.
-          </p>
-        </div>
+          <FAQItem question="Cancellation and Refunds">
+            <p class="mb-4">
+              Short version: We only want your money if you are happy.
+            </p>
+            <p class="mb-4">
+              Long version: If you need to cancel your membership for any
+              reason, please <a class="link" href={DISCORD_URL}
+                >contact @noaxis on Discord</a
+              >. If you cancel within the first 5 days of a signup or renewal,
+              we'll refund the full amount. Otherwise, we'll pro-rate it. Sound
+              good?
+            </p>
+            <p>
+              If you create additional instances and then downgrade to the free
+              plan, the extra instances will remain accessible in your
+              dashboard, but they will not run.
+            </p>
+          </FAQItem>
+
+          <!-- More questions... -->
+        </dl>
       </div>
-    </div>
+    </main>
   </div>
 </AuthStateGuard>
---
+++ frontends/dashboard/src/routes/account/PricingCard.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+  import { PLAN_NAMES, SubscriptionType } from '$shared'
+
+  export let name = ''
+  export let description = ''
+  export let priceMonthly = 0
+  export let priceAnnually = 0
+  export let planSchedule = ''
+  export let checkoutMonthURL = ''
+  export let checkoutYearURL = ''
+  export let active = false
+  export let features: string[] = []
+</script>
+
+<div
+  class={`rounded-3xl p-8 xl:p-10 ${
+    active ? 'bg-white/5 ring-2 ring-primary' : 'ring-1 ring-white/10'
+  }`}
+>
+  <div class="flex items-center justify-between gap-x-4">
+    <h2 id="tier-startup" class="text-lg font-semibold leading-8 text-white">
+      {name}
+    </h2>
+
+    {#if active}
+      <p
+        class="rounded-full bg-indigo-500 px-2.5 py-1 text-xs font-semibold leading-5 text-white"
+      >
+        Your Plan
+      </p>
+    {/if}
+  </div>
+
+  <p class="mt-4 text-sm leading-6 text-gray-300">{description}</p>
+
+  <p class="mt-6 flex items-baseline gap-x-1">
+    {#if priceMonthly === 0}
+      <span class="text-4xl font-bold tracking-tight text-white">Free</span>
+    {:else if priceMonthly === priceAnnually}
+      <span class="text-4xl font-bold tracking-tight text-white">$300</span>
+      <span class="text-sm font-semibold leading-6 text-gray-300">
+        one time</span
+      >
+    {:else}
+      <span class="text-4xl font-bold tracking-tight text-white">
+        {#if planSchedule === 'month'}
+          ${priceMonthly}
+        {:else}
+          ${priceAnnually}
+        {/if}
+      </span>
+      <span class="text-sm font-semibold leading-6 text-gray-300">
+        / {planSchedule}</span
+      >
+    {/if}
+  </p>
+
+  {#if !active}
+    <a
+      href={planSchedule === 'month' ? checkoutMonthURL : checkoutYearURL}
+      aria-describedby="tier-freelancer"
+      class="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white"
+      >Switch to this plan</a
+    >
+  {/if}
+
+  <ul
+    role="list"
+    class="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10"
+  >
+    {#each features as feature}
+      <li class="flex items-center gap-x-3">
+        <i class="fa-light fa-check"></i>
+        {feature}
+      </li>
+    {/each}
+  </ul>
+</div>
