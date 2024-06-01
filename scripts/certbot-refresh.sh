bc87802120789fff5cdbd7536a5cbde5a2e9ce6e
Ben Allfree <ben@benallfree.com>
2024-06-01 09:54:10 -0400

    chore(root): localize helper scripts

--- scripts/certbot-refresh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# https://www.digitalocean.com/community/tutorials/how-to-create-let-s-encrypt-wildcard-certificates-with-certbot
-certbot renew --dns-digitalocean --dns-digitalocean-credentials ~/certbot-creds.ini  
-cp /etc/letsencrypt/live/pockethost.io/fullchain.pem ./ssl
-cp /etc/letsencrypt/live/pockethost.io/privkey.pem ./ssl
-chown pockethost:pockethost -R ./ssl
--- scripts/live-sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Used to back up to an encrypted drive
-rsync -avz --progress --exclude 'logs.db' --exclude 'instance_logs.db' --exclude '*.db-shm' --exclude '*.db-wal' --exclude 'pb_data/types.d.ts'  pockethost.io:/home/pockethost/data/ .pockethost/live-data
