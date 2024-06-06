6abcbfe5d3952ffc6a74c9fd0fa33fef6e1ba331
Ben Allfree <ben@benallfree.com>
2024-06-05 17:59:05 -0700

    chore(lander): set apex domain default to local

--- packages/lander/_data/env.js
+++ packages/lander/_data/env.js
@@ -1,18 +1,18 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config({ path: '.env' })
-}
-
 /**
  * These environment variables default to pointing to the production build so
  * frontend development is easy. If they are specified in .env, those values
  * will prevail.
  */
 
+const PUBLIC_APEX_DOMAIN = process.env.PUBLIC_APEX_DOMAIN || 'pockethost.lvh.me'
+
 const env = {
   // The domain name where this dashboard lives
-  PUBLIC_APP_URL: process.env.PUBLIC_APP_URL || 'https://app.pockethost.io',
+  PUBLIC_APP_URL:
+    process.env.PUBLIC_APP_URL || `https://app.${PUBLIC_APEX_DOMAIN}`,
 
-  PUBLIC_BLOG_URL: process.env.PUBLIC_BLOG_URL || 'https://pockethost.io',
+  PUBLIC_BLOG_URL:
+    process.env.PUBLIC_BLOG_URL || `https://${PUBLIC_APEX_DOMAIN}`,
 
   PUBLIC_DISCORD_URL: `https://discord.gg/HsSjcuPRWX`,
 }
