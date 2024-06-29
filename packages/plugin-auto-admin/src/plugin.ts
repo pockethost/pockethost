b2d33cfdbbcfd3165dbbaea685bd03446af96efc
Ben Allfree <ben@benallfree.com>
2024-06-29 13:54:16 -0700

    chore(auto-admin): plugin.ts

---
+++ packages/plugin-auto-admin/src/plugin.ts
@@ -0,0 +1,28 @@
+import PocketBase from 'pocketbase'
+import {
+  PocketHostPlugin,
+  onAfterInstanceStartedAction,
+  parseError,
+} from 'pockethost'
+import { LOGIN, PASSWORD, PLUGIN_NAME } from './constants'
+import { dbg, info } from './log'
+
+export const plugin: PocketHostPlugin = async ({}) => {
+  dbg(`initializing ${PLUGIN_NAME}`)
+
+  onAfterInstanceStartedAction(async ({ instance, url }) => {
+    const { subdomain } = instance
+    dbg(`launched instance ${instance.subdomain} on ${url}`)
+    const client = new PocketBase(url)
+    const email = LOGIN()
+    const password = PASSWORD()
+    try {
+      await client.admins.create({ email, password, passwordConfirm: password })
+      dbg(`Admin account created`)
+    } catch (e: any) {
+      dbg(`Failed to create admin account: ${parseError(e)} `)
+    } finally {
+      info(`Default admin login for ${subdomain} is ${email}/${password}`)
+    }
+  })
+}
