7b8248a486d43f5e59fe8261cf4731a18565a4d1
Ben Allfree <ben@benallfree.com>
2024-06-29 13:48:30 -0700

    chore(multi): move to plugin.ts

---
+++ packages/plugin-cloudflare-request-logger/src/plugin.ts
@@ -0,0 +1,23 @@
+import { info } from 'console'
+import { PocketHostPlugin, onIncomingRequestAction } from 'pockethost'
+import { PLUGIN_NAME } from './constants'
+import { dbg } from './log'
+
+export const plugin: PocketHostPlugin = async ({}) => {
+  dbg(`initializing ${PLUGIN_NAME}`)
+
+  onIncomingRequestAction(async ({ req, res }) => {
+    const url = new URL(`http://${req.headers.host}${req.url}`)
+    const country = (req.headers['cf-ipcountry'] as string) || '<ct>'
+    const ip = (req.headers['x-forwarded-for'] as string) || '<ip>'
+    const method = req.method || '<m>'
+    const sig = [
+      method.padStart(10),
+      country.padStart(5),
+      ip.padEnd(45),
+      url.toString(),
+    ].join(' ')
+    info(`Incoming request ${sig}`)
+    res.locals.sig = sig
+  })
+}
