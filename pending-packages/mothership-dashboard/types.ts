873fcd1074be74f8a7b4739e75e93001eff2c86e
Ben Allfree <ben@benallfree.com>
2024-06-29 17:25:31 -0700

    chore(root): cleanup

--- pending-packages/mothership-dashboard/types.ts
+++ pending-packages/mothership-dashboard/types.ts
@@ -1,18 +1,11 @@
-export type RecordId = string
 export type UserId = RecordId
 export type InstanceId = RecordId
 export type Semver = string
 export type InternalInstanceId = RecordId
 export type Subdomain = string
 export type Port = number
-export type IsoDate = string
 export type ProcessId = number
 export type Username = string
 export type Password = string
 export type CollectionName = string
 export type Seconds = number
-export type BaseFields = {
-  id: RecordId
-  created: IsoDate
-  updated: IsoDate
-}
