331cdc9fd56ab755c6ba325382a9ffe0fc43fb64
Ben Allfree <ben@benallfree.com>
2024-07-30 09:50:23 -0400

    chore(pockethost): gobot service fixes (split commit)

--- packages/pockethost/src/constants.ts
+++ packages/pockethost/src/constants.ts
@@ -149,6 +149,8 @@ export const SETTINGS = {
   DISCORD_HEALTH_CHANNEL_URL: mkString(''),
 
   DOCKER_CONTAINER_HOST: mkString(`host.docker.internal`),
+
+  PH_GOBOT_ROOT: mkPath(join(_PH_HOME, 'gobot'), { create: true }),
 }
 
 export type Settings = ReturnType<typeof DefaultSettingsService>
@@ -277,6 +279,9 @@ export const DISCORD_HEALTH_CHANNEL_URL = () =>
   settings().DISCORD_HEALTH_CHANNEL_URL
 
 export const DOCKER_CONTAINER_HOST = () => settings().DOCKER_CONTAINER_HOST
+
+export const PH_GOBOT_ROOT = (...paths: string[]) =>
+  join(settings().PH_GOBOT_ROOT, ...paths)
 
 /** Helpers */
 
