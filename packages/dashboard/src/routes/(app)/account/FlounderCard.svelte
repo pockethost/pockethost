ebbc981cd34630aa79c5b3a6116a0edb7462ac88
Ben Allfree <ben@benallfree.com>
2024-10-10 00:33:47 -0700

    wip

--- packages/dashboard/src/routes/(app)/account/FlounderCard.svelte
+++ packages/dashboard/src/routes/(app)/account/FlounderCard.svelte
@@ -17,8 +17,8 @@
   description="Epic elite! The Flounder's Edition is almost as good as the Founder's edition, and you'll help PocketHost go global"
   priceMonthly={[359, 'once, use forever', 299]}
   priceAnnually={[159, 'year (save 55%)', 99]}
-  checkoutMonthURL="https://store.pockethost.io/buy/82d79f7c-64f6-4c2b-9f58-dcc8951f1cdd?checkout[custom][user_id]={$userStore?.id}&checkout[email]={$userStore?.email}&checkout[discount_code]=FLPRESALE24"
-  checkoutYearURL="https://store.pockethost.io/buy/9ff8775b-6b9e-4aa8-a0ab-dc5e58e25408?checkout[custom][user_id]={$userStore?.id}&checkout[email]={$userStore?.email}&checkout[discount_code]=FLPRESALE24"
+  checkoutMonthURL="https://store.pockethost.io/buy/9ff8775b-6b9e-4aa8-a0ab-dc5e58e25408?checkout[custom][user_id]={$userStore?.id}&checkout[email]={$userStore?.email}&checkout[discount_code]=FLPRESALE24"
+  checkoutYearURL="https://store.pockethost.io/buy/82d79f7c-64f6-4c2b-9f58-dcc8951f1cdd?checkout[custom][user_id]={$userStore?.id}&checkout[email]={$userStore?.email}&checkout[discount_code]=FLPRESALE24"
   features={[
     `Everything in the ${PLAN_NAMES[SubscriptionType.Premium]} tier`,
     `Commemorative Flounder's badge`,
