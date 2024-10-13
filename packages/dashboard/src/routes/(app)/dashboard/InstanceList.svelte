ebc8aef1eb342d19800efcdee138674716d3d189
Virya Dharmawan <viryadharmawan@gmail.com>
2024-10-13 23:55:31 +0700

    Dashboard Revert
    Revert c65551795f6a7ca5d8edaf2833180f9c0a5dfc82 and fix card width

--- packages/dashboard/src/routes/(app)/dashboard/InstanceList.svelte
+++ packages/dashboard/src/routes/(app)/dashboard/InstanceList.svelte
@@ -24,55 +24,53 @@
 </script>
 
 {#each values($globalInstancesStore).sort( (a, b) => a.subdomain.localeCompare(b.subdomain), ) as instance, index}
-  <div class="w-80">
-    <button
-      class={`card flex-1 m-4 transition hover:bg-base-300 ${instance.maintenance ? 'bg-base-200' : 'bg-neutral'}`}
-      on:click={_=>goto(`/instances/${instance.id}`)}
-    >
-      <div class="card-body">
-        <div class="card-title">
-          <div class="flex justify-between items-center w-full">
-            <span>{instance.subdomain}</span>
-            <input
-              type="checkbox"
-              class="toggle {instance.maintenance
-                ? 'bg-red-500 hover:bg-red-500'
-                : 'toggle-success'}"
-              checked={!instance.maintenance}
-              on:click={e=>e.stopPropagation()}
-              on:change={handleMaintenanceChange(instance.id)}
-            />
-          </div>
-        </div>
-        <p class="text-left">
-          <span class="text-gray-400"
-            >Version {instance.version}
-            <span class={!instance.maintenance ? 'hidden' : ''}
-              >- Powered Off</span
-            ></span
-          >
-        </p>
-
-        <div class="card-actions flex justify-between mt-5">
-          <a href={`/instances/${instance.id}`} class="btn btn-primary">
-            <Fa icon={faCircleInfo} />
-            <span>Details</span>
-          </a>
-
-          <a
-            class="btn btn-secondary"
-            href={INSTANCE_ADMIN_URL(instance)}
-            target="_blank"
-          >
-            <img
-              src="/images/pocketbase-logo.svg"
-              alt="PocketBase Logo"
-              class="w-6"
-            />
-            <span>Admin</span>
-          </a>
+  <button
+    class={`card min-w-80 flex-1 m-4 transition hover:bg-base-300 ${instance.maintenance ? 'bg-base-200' : 'bg-neutral'}`}
+    on:click={_=>goto(`/instances/${instance.id}`)}
+  >
+    <div class="card-body w-full">
+      <div class="card-title">
+        <div class="flex justify-between items-center w-full">
+          <span>{instance.subdomain}</span>
+          <input
+            type="checkbox"
+            class="toggle {instance.maintenance
+              ? 'bg-red-500 hover:bg-red-500'
+              : 'toggle-success'}"
+            checked={!instance.maintenance}
+            on:click={e=>e.stopPropagation()}
+            on:change={handleMaintenanceChange(instance.id)}
+          />
         </div>
       </div>
-    </button>
-  </div>
+      <p class="text-left">
+        <span class="text-gray-400"
+          >Version {instance.version}
+          <span class={!instance.maintenance ? 'hidden' : ''}
+            >- Powered Off</span
+          ></span
+        >
+      </p>
+
+      <div class="card-actions flex justify-between mt-5">
+        <a href={`/instances/${instance.id}`} class="btn btn-primary">
+          <Fa icon={faCircleInfo} />
+          <span>Details</span>
+        </a>
+
+        <a
+          class="btn btn-secondary"
+          href={INSTANCE_ADMIN_URL(instance)}
+          target="_blank"
+        >
+          <img
+            src="/images/pocketbase-logo.svg"
+            alt="PocketBase Logo"
+            class="w-6"
+          />
+          <span>Admin</span>
+        </a>
+      </div>
+    </div>
+  </button>
 {/each}
