cc74a73b4e2f8ee99839a5a339cf83c2dffc1585
Ben Allfree <ben@benallfree.com>
2024-10-16 14:51:37 -0700

    pricing cosmetic fixes

--- packages/dashboard/src/routes/(static)/pricing/PricingCard.svelte
+++ packages/dashboard/src/routes/(static)/pricing/PricingCard.svelte
@@ -112,7 +112,7 @@
       </div>
     {/if}
 
-    <div class="mt-4 mb-4 leading-6 text-gray-300">{description}</div>
+    <div class="mt-4 mb-4 leading-6 text-gray-300 text-left">{description}</div>
 
     <div class="flex flex-col gap-y-2 mb-4">
       {#each features as feature}
@@ -130,8 +130,8 @@
     {#if fundingGoals.length > 0}
       <div class="flex flex-col gap-y-2 mb-4">
         <div>Feature Goals</div>
-        <div class="text-sm">
-          When Flounders sell out, we'll use the funds to achieve these goals.
+        <div class="text-sm text-left">
+          When {name} sell out, we'll use the funds to achieve these goals.
         </div>
         {#each fundingGoals as goal}
           <div class="text-gray-300 flex items-center text-sm space-x-2">
@@ -141,15 +141,9 @@
         {/each}
       </div>
     {/if}
-    <div class="card-actions">
-      {#if priceMonthly[0] === 0}
-        <p class="mt-6 mb-12 flex items-baseline gap-x-1">
-          <span class="text-4xl font-bold tracking-tight text-white">Free</span>
-        </p>
-      {/if}
 
+    <div class="card-actions">
       {#if priceAnnually[0] > 0}
-        {console.log('priceAnnually', priceAnnually)}
         <a
           href={comingSoon ? undefined : checkoutMonthURL}
           class={`btn ${qtyRemaining <= 0 ? 'btn-disabled' : ''} w-full rounded-md focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-white/10 hover:bg-white/20 focus-visible:outline-white `}
