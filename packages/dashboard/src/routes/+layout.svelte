13dc94b1e4ceeb5e525d730f55ba5c314ee36dc5
Ben Allfree <ben@benallfree.com>
2024-10-18 07:34:10 -0700

    cookie consent & analytics

--- packages/dashboard/src/routes/+layout.svelte
+++ packages/dashboard/src/routes/+layout.svelte
@@ -5,6 +5,9 @@
   import '../app.css'
   import { onMount } from 'svelte'
   import { init } from '$util/stores'
+  import '@beyonk/gdpr-cookie-consent-banner/banner.css' // optional, you can also define your own styles
+  // @ts-ignore
+  import GdprBanner from '@beyonk/gdpr-cookie-consent-banner'
 
   onMount(() => {
     init()
@@ -31,4 +34,24 @@
   </div>
   <div>(c) 2024, PocketHost</div>
   <div>Proudly hacking open source in Reno, NV</div>
+</div>
+<GdprBanner
+  cookieName="pockethost_gpdr"
+  description="PocketHost uses cookies to ensure you get the best experience."
+/>
+<div>
+  <!-- Google tag (gtag.js) -->
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-5Q6CM5HPCX"
+  ></script>
+  <script>
+    window.dataLayer = window.dataLayer || []
+    function gtag() {
+      dataLayer.push(arguments)
+    }
+    gtag('js', new Date())
+
+    gtag('config', 'G-5Q6CM5HPCX')
+  </script>
 </div>
