d0807437c07d7b1b256b8cd850c81d93d90dace9
Ben Allfree <ben@benallfree.com>
2024-12-20 10:22:09 +0000

    fix: bad mirror data hook path

--- packages/pockethost/src/mothership-app/src/lib/handlers/mirror/hooks.ts
+++ packages/pockethost/src/mothership-app/src/lib/handlers/mirror/hooks.ts
@@ -2,7 +2,7 @@ routerAdd(
   'GET',
   '/api/mirror',
   (c) => {
-    return require(`${__hooks}/HandleMirrorData`).HandleMirrorData(c)
+    return require(`${__hooks}/mothership`).HandleMirrorData(c)
   },
   $apis.gzip(),
   $apis.requireAdminAuth(),
