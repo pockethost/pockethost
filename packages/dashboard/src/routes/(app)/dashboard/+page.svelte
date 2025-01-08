45208df304d138996abcc6ab03a12723993fab87
Ben Allfree <ben@benallfree.com>
2025-01-08 12:06:00 -0800

    chore: dashboard page cleanup

--- packages/dashboard/src/routes/(app)/dashboard/+page.svelte
+++ packages/dashboard/src/routes/(app)/dashboard/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-  import { MAX_INSTANCE_COUNTS } from '$src/env'
-  import { globalInstancesStore, userSubscriptionType } from '$util/stores'
-  import { values } from '@s-libs/micro-dash'
   import Main from './Main.svelte'
-
-  $: maxInstanceCount = MAX_INSTANCE_COUNTS[$userSubscriptionType]
-  $: instanceCount = values($globalInstancesStore).length
 </script>
 
 <Main />
