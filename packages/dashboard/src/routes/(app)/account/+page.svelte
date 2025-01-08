2b243ef59748b9b5db98f09d2fbf9d571c7c9d80
Ben Allfree <ben@benallfree.com>
2025-01-08 12:01:13 -0800

    feat: show email in account settings

--- packages/dashboard/src/routes/(app)/account/+page.svelte
+++ packages/dashboard/src/routes/(app)/account/+page.svelte
@@ -2,13 +2,15 @@
   import { userSubscriptionType } from '$util/stores'
   import { PLAN_NAMES, SubscriptionType } from 'pockethost/common'
   import { userStore } from '$util/stores'
-  import { onMount } from 'svelte'
-  import FlounderCard from './FlounderCard.svelte'
   import Avatar from '$src/routes/Navbar/Avatar.svelte'
 </script>
 
 <div class="p-4">
   <div class="text-2xl m-4">My Account</div>
+
+  <div class="m-4">
+    Your email: <span class="text-success font-bold">{$userStore?.email}</span>
+  </div>
   <div class="m-4">
     Your plan: <span class="text-success font-bold"
       >{PLAN_NAMES[$userSubscriptionType]}</span
