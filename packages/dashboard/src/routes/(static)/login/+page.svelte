265e653f1a0f3aa05f73ccf510151991ebad3850
cheskoxd <franchesko77lacayo@gmail.com>
2025-08-02 16:26:19 -0500

    fix(login): center InstanceGeneratorWidget within the container

--- packages/dashboard/src/routes/(static)/login/+page.svelte
+++ packages/dashboard/src/routes/(static)/login/+page.svelte
@@ -3,6 +3,6 @@
   import InstanceGeneratorWidget from '../get-started/InstanceGeneratorWidget.svelte'
 </script>
 
-<div class="w-full px-10 md:px-16 my-12">
+<div class="w-full flex items-center justify-center px-10 md:px-16 my-12">
   <InstanceGeneratorWidget login />
 </div>
