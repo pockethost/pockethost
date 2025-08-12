b02012c8976263c2f222eff21eabc01085357501
cheskoxd <franchesko77lacayo@gmail.com>
2025-08-11 22:08:39 -0500

    style(docsSideBar): Separate scroll between sidebar and content, also stick sidebar to top

--- packages/dashboard/src/routes/(static)/docs/+layout.svelte
+++ packages/dashboard/src/routes/(static)/docs/+layout.svelte
@@ -7,57 +7,66 @@
 </script>
 
 <BlurBg className="opacity-20 blur-2xl" />
-<div class="container-fluid mt-4 flex gap-6 relative z-10 flex-col-reverse md:flex-row px-4 md:px-20">
-  <div class="w-full md:w-fit py-4">
-    <ul class="menu text-base">
-      <li class="menu-title">Overview</li>
-      <DocLink path="introduction" title="Introduction" />
-      <DocLink path="getting-started" title="Getting Started" />
-    </ul>
-    <ul class="menu">
-      <li class="menu-title">Instance Management</li>
-      <DocLink path="create" title="Create" />
-      <DocLink path="accessing" title="Connect" />
-      <DocLink path="power" title="Power" />
-      <DocLink path="rename-instance" title="Rename" />
-      <DocLink path="delete" title="Delete" />
-      <DocLink path="limits" title="Limits" />
-      <DocLink path="smtp" title="Outgoing Email" />
-      <DocLink path="versions" title="Changing Versions" />
-      <DocLink path="s3" title="Using S3 Storage" />
-      <DocLink path="custom-binaries" title="Custom Binaries" />
-      <DocLink path="custom-domains" title="Custom Domains" />
-      <DocLink path="backup-restore" title="Backup/Restore" />
-    </ul>
-    <ul class="menu">
-      <li class="menu-title">Daily Use Guide</li>
-      <DocLink path="logs" title="Logging" />
-      <DocLink path="dev-mode" title="Dev Mode" />
-      <DocLink path="secrets" title="Secrets" />
-      <DocLink path="webhooks" title="Webhooks" />
-      <DocLink path="ftp" title="FTP Access" />
-      <DocLink path="admin-sync" title="Admin Sync" />
-      <DocLink path="js" title="Extending via JS" />
-    </ul>
-    <ul class="menu">
-      <li class="menu-title">Programming Guide</li>
-      <DocLink path="programming" title="Frontends and JS Hooks" />
-      <DocLink path="server-side-pocketbase-antipattern" title="Server-Side PocketBase is an Anti-Pattern" />
-    </ul>
-    <ul class="menu">
-      <li class="menu-title">Appendix</li>
-      <DocLink path="account-creation" title="Account Creation" />
-      <DocLink path="pricing-ethos" title="Pricing Ethos" />
-      <DocLink path="faq" title="FAQ" />
-      <DocLink path="gs-gmail" title="Google Suite Gmail Setup" />
-      <DocLink path="ses" title="Amazon SES Setup" />
-      <DocLink path="self-hosting" title="Self-Hosting" />
-    </ul>
-  </div>
-  <div class="docs-content prose p-5 w-full md:w-fit md:max-w-[50%]">
-    <slot />
+
+<div class="px-4 md:px-20">
+  <div class="mt-4 flex gap-6 z-10 relative flex-col-reverse md:flex-row">
+    <!-- Sidebar -->
+    <div class="min-w-48 h-screen sticky top-0 overflow-y-auto">
+      <div class="w-full py-4">
+        <ul class="menu text-base">
+          <li class="menu-title">Overview</li>
+          <DocLink path="introduction" title="Introduction" />
+          <DocLink path="getting-started" title="Getting Started" />
+        </ul>
+        <ul class="menu">
+          <li class="menu-title">Instance Management</li>
+          <DocLink path="create" title="Create" />
+          <DocLink path="accessing" title="Connect" />
+          <DocLink path="power" title="Power" />
+          <DocLink path="rename-instance" title="Rename" />
+          <DocLink path="delete" title="Delete" />
+          <DocLink path="limits" title="Limits" />
+          <DocLink path="smtp" title="Outgoing Email" />
+          <DocLink path="versions" title="Changing Versions" />
+          <DocLink path="s3" title="Using S3 Storage" />
+          <DocLink path="custom-binaries" title="Custom Binaries" />
+          <DocLink path="custom-domains" title="Custom Domains" />
+          <DocLink path="backup-restore" title="Backup/Restore" />
+        </ul>
+        <ul class="menu">
+          <li class="menu-title">Daily Use Guide</li>
+          <DocLink path="logs" title="Logging" />
+          <DocLink path="dev-mode" title="Dev Mode" />
+          <DocLink path="secrets" title="Secrets" />
+          <DocLink path="webhooks" title="Webhooks" />
+          <DocLink path="ftp" title="FTP Access" />
+          <DocLink path="admin-sync" title="Admin Sync" />
+          <DocLink path="js" title="Extending via JS" />
+        </ul>
+        <ul class="menu">
+          <li class="menu-title">Programming Guide</li>
+          <DocLink path="programming" title="Frontends and JS Hooks" />
+          <DocLink path="server-side-pocketbase-antipattern" title="Server-Side PocketBase is an Anti-Pattern" />
+        </ul>
+        <ul class="menu">
+          <li class="menu-title">Appendix</li>
+          <DocLink path="account-creation" title="Account Creation" />
+          <DocLink path="pricing-ethos" title="Pricing Ethos" />
+          <DocLink path="faq" title="FAQ" />
+          <DocLink path="gs-gmail" title="Google Suite Gmail Setup" />
+          <DocLink path="ses" title="Amazon SES Setup" />
+          <DocLink path="self-hosting" title="Self-Hosting" />
+        </ul>
+      </div>
+    </div>
+
+    <!-- Main Content -->
+    <div class="docs-content prose p-5 w-full md:w-fit md:max-w-[50%]">
+      <slot />
+    </div>
   </div>
 </div>
+
 
 <style lang="scss">
   .menu {
